6714a48efb815023b709245f40a41e64739cc465
Moudjoungou Moukagni <jourdontmm@yahoo.com>
2024-09-05 17:23:47 +0200

    modification

--- Exo-kotlin/Exo-Kotlin-master/09-Sortie_standard/P09_b-Corriger_code/src/main/kotlin/main.kt
+++ Exo-kotlin/Exo-Kotlin-master/09-Sortie_standard/P09_b-Corriger_code/src/main/kotlin/main.kt
@@ -1,9 +1,20 @@
-fun main() {
-    println(1, 2, 3...)
-    printLin("Line print here ")
-    lnPrint(42)
-    printLn("Pay attention to")
+ fun main() {
+    println("1, 2, 3...")
+    print("Line print here ")
+    println(42)
+    print("Pay attention to ")
     println("syntax")
 }
+
+/* fun main(){
+    for (i in 1..3){
+        print("$i,")
+    }
+    println("...")
+
+    println("Line print here 42")
+
+    println("Pay attention to syntax")
+} */
 
 
